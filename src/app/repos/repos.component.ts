84b23dd118ffc3450b49acdf15f1cc427a0817b1
Marvin Zachary <acerzach@gmail.com>
2019-04-03 23:29:15 +0300

    Added the ts for repos

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,27 +1,27 @@
-// import { Component, OnInit } from '@angular/core';
-// import { SService } from '../s.service';
-// @Component({
-//   selector: 'app-menu',
-//   templateUrl: './repos.component.html',
-//   styleUrls: ['./repos.component.scss']
-// })
-// export class ReposComponent implements OnInit {
+import { Component, OnInit } from '@angular/core';
+import { SService } from '../s.service';
+@Component({
+  selector: 'app-menu',
+  templateUrl: './repos.component.html',
+  styleUrls: ['./repos.component.scss']
+})
+export class ReposComponent implements OnInit {
 
-//   profile: any = [];
-//   repos: any = [];
-//   username: string;
-//   constructor(private detailspageServe: SService) { }
-//   ngOnInit() {
-//   }
-//   findprofile() {
-//     this.detailspageServe.updateprofile(this.username);
-//     this.detailspageServe.getprofileInfo(this.username).subscribe(profile => {
-//       console.log(profile);
-//       this.profile = profile;
-//     });
-//     this.detailspageServe.getprofileRepos().subscribe(repos => {
-//       console.log(repos);
-//       this.repos = repos;
-//     });
-//   }
-// }
+  profile: any = [];
+  repos: any = [];
+  username: string;
+  constructor(private detailspageServe: SService) { }
+  ngOnInit() {
+  }
+  findprofile() {
+    this.detailspageServe.updateprofile(this.username);
+    this.detailspageServe.getprofileInfo(this.username).subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.detailspageServe.getprofileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
+}
