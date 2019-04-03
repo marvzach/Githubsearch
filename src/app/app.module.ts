21c51797191386c44afd1485ab8b83a76fb6aece
Marvin Zachary <acerzach@gmail.com>
2019-04-03 23:33:41 +0300

    Added  every component , modeles and services in the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,34 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-// import { SService} from './s.service';
-// import { NO_ERRORS_SCHEMA } from '@angular/core';
-// import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { SService } from './s.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
-// import { AppComponent } from './app.component';
-// import { AppRoutingModule } from './app-routing/app-routing.module';
-// import { HeaderComponent } from './header/header.component';
-// import { ReposComponent } from './repos/repos.component';
-// import { UserComponent } from './user/user.component';
-// import {FormsModule} from '@angular/forms';
-// import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { ReposComponent } from './repos/repos.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     HeaderComponent,
-//     ReposComponent,
-//     UserComponent,
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     MDBBootstrapModule.forRoot(),
-//     FormsModule,
-//     HttpClientModule
-//   ],
-//   schemas: [NO_ERRORS_SCHEMA],
-//   providers: [SService],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        ReposComponent,
+        UserComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MDBBootstrapModule.forRoot(),
+        FormsModule,
+        HttpClientModule
+    ],
+    schemas: [NO_ERRORS_SCHEMA],
+    providers: [SService],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
