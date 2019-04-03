d5ab05a9526fc7de99af15826818ecd50c736043
Marvin Zachary <acerzach@gmail.com>
2019-04-03 23:43:19 +0300

    Added imports and paths for accessing diffrent components on the UI

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,11 +1,11 @@
-// import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
-// import { ReposComponent} from '../repos/repos.component';
-// import { UserComponent } from '../user/user.component';
+import { ReposComponent } from '../repos/repos.component';
+import { UserComponent } from '../user/user.component';
 
 
-// export const routes: Routes = [
-//     { path: 'home',  component: UserComponent },
-//     { path: 'repos',     component: ReposComponent },
-//     { path: '', redirectTo: '/home', pathMatch: 'full' }
-//   ];
+export const routes: Routes = [
+    { path: 'home', component: UserComponent },
+    { path: 'repos', component: ReposComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
